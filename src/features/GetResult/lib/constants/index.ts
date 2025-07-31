51c8de0923f80f34e4530ec2088317317fa4461c
Sergey Gorbatenko <siarheimjxfull@gmail.com>
2025-07-31 05:51:44 +0300

    refactor(links): props with broken types

--- src/features/GetResult/lib/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const CGenerateButtons = ['GENERATE', 'RANDOM'];
--- src/shared/ui/Link/lib/utils/useLinkType.ts
+++ src/shared/ui/Link/lib/utils/useLinkType.ts
@@ -1,15 +1,11 @@
-import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import type { ILink } from '../../model/interfaces';
 import type { TLinkStyle } from '../../model/types';
 import { CLinkStylesNames } from '../constants';
 
-export const useLinkType = ({ to, type }: ILink): TLinkStyle => {
+export const useLinkType = ({ to, styleType }: ILink): TLinkStyle => {
   const { pathname } = useLocation();
 
-  return useMemo(
-    () => (pathname === to ? CLinkStylesNames.ACTIVE : type),
-    [pathname, to, type]
-  );
+  return pathname === to ? CLinkStylesNames.ACTIVE : styleType;
 };
